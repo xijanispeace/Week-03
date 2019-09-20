728ed5d41c75f968a6923447da891b95a47a6bb2
WidsawaD <safefire@gmail.com>
2019-09-20 15:02:42 +0700

    Edit aGain

---
+++ 61030243/CL62-03/CL62-03/CL62-03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL62_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Computer Lab");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 61030243/Engedu/Engedu/Engedu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engedu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 2561 semester 1, I am an Engedu freshman.");
+            Console.WriteLine("Year 2561 semester 2, I learn to program in c sharp.");
+
+            Console.WriteLine("Year 2562 semester 1, I have to regrade on some course.");
+
+            Console.WriteLine("Year 2562 semester 1, I start my programmer life.");
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
