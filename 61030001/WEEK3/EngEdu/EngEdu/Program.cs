a5e78a654a5679377868c92a8bc14b11b4f20cb1
Krungthai Sumprung <53931475+aomnaherb@users.noreply.github.com>
2019-09-20 14:10:12 +0700

    Commit AomNaherb Lab3
    ส่งงานLab ครับมีงานและไฟล์ ExamWeek3 ที่เป็นใบงานครับ

---
+++ 61030001/WEEK3/EngEdu/EngEdu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semester 1, I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semester 2, I learn to program in C shrp.");
+            Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
+        }
+    }
+}
