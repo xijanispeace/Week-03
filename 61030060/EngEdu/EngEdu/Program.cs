cbd983aef442d0c08d3ea8077a8e8ec75641d014
SuphonSaejia <53931544+SuphonSaejia@users.noreply.github.com>
2019-10-03 16:45:43 +0700

    Add files via upload

---
+++ 61030060/EngEdu/EngEdu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x  semester 1, I am an EngEdu freshman");
+            Console.WriteLine("Year 255x  semester 2, I learn to program in C#");
+<<<<<<< HEAD
+            Console.WriteLine("Year 2560  semester 1, I  have to regrade on some course");
+
+=======
+            Console.WriteLine("Year 255x  semester 1, I start   my programer life ");
+>>>>>>> Computer
+        }
+    }
+}
