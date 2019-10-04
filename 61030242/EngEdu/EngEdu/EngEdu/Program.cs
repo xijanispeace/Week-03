86aee09c84ab4ff465fe4add0425ef4a9df83f0a
baitoeii <53931538+baitoeii@users.noreply.github.com>
2019-10-04 15:28:49 +0700

    Add files via upload

---
+++ 61030242/EngEdu/EngEdu/EngEdu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semester 1, I am an Engedu Freshman.");
+            Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
+            Console.WriteLine("Year 2560 semester 1, I have regrade on some course.");
+
+        }
+    }
+}
